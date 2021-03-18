a847d59f89acf29d1fdb079d9b5caf3929bdb836
WSixx <lucas.2007s@gmail.com>
2021-03-18 14:11:12 -0300

    tabuleiro widget

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_minesweeper/components/campo_widget.dart';
 import 'package:flutter_minesweeper/components/resultado_widget.dart';
+import 'package:flutter_minesweeper/components/tabuleiro_widget.dart';
 import 'package:flutter_minesweeper/models/campo.dart';
-import 'package:flutter_minesweeper/models/explosao_exception.dart';
+import 'package:flutter_minesweeper/models/tabuleiro.dart';
 
-class CampoMinadoApp extends StatelessWidget {
+class CampoMinadoApp extends StatefulWidget {
+  @override
+  _CampoMinadoAppState createState() => _CampoMinadoAppState();
+}
+
+class _CampoMinadoAppState extends State<CampoMinadoApp> {
+  bool _venceu;
+  Tabuleiro _tabuleiro = Tabuleiro(linhas: 12, colunas: 12, qtdBombas: 3);
   void _reiniciar() {}
+
   void _abrir(Campo campo) {}
+
   void _alternarMarcacao(Campo campo) {}
 
   @override
   Widget build(BuildContext context) {
-    Campo campo = Campo(linha: 0, coluna: 0);
-    try {
-      campo.minar();
-      campo.abrir();
-    } on ExplosaoException {}
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
-          venceu: true,
+          venceu: _venceu,
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: CampoWidget(
-            campo: campo,
+          child: TabuleiroWidget(
             onAbrir: _abrir,
             onAlternarMarcacao: _alternarMarcacao,
+            tabuleiro: _tabuleiro,
           ),
         ),
       ),
